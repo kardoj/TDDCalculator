6660c6fd1d4e292086d88e6681430822e8f5ac2f
Kardo <kardoj@gmail.com>
2016-05-03 00:08:43 +0300

    Refactored ugly button creation

--- src/main/java/xyz/kardo/calculator/CalculatorGui.java
+++ src/main/java/xyz/kardo/calculator/CalculatorGui.java
@@ -8,6 +8,8 @@ package xyz.kardo.calculator;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,6 +21,8 @@ public class CalculatorGui implements MouseListener{
 	Calculator calculator;
 	JFrame window;
 	JTextField outputField;
+	String buttonLabelsInOrder = "123+456-789*C0=/";
+	List<JButton> buttons = new ArrayList<JButton>();
 	
 	public CalculatorGui(){
 		window = new JFrame("Calculator");
@@ -40,56 +44,15 @@ public class CalculatorGui implements MouseListener{
 
 	private void createButtons(JPanel frame) {
 		JPanel buttonPanel = new JPanel(new GridLayout(4, 4));
-		JButton oneButton = new JButton("1");
-		JButton twoButton = new JButton("2");
-		JButton threeButton = new JButton("3");
-		JButton addButton = new JButton("+");
-		JButton fourButton = new JButton("4");
-		JButton fiveButton = new JButton("5");
-		JButton sixButton = new JButton("6");
-		JButton subtractButton = new JButton("-");
-		JButton sevenButton = new JButton("7");
-		JButton eightButton = new JButton("8");
-		JButton nineButton = new JButton("9");
-		JButton multiplyButton = new JButton("*");
-		JButton resetButton = new JButton("C");
-		JButton zeroButton = new JButton("0");
-		JButton calculateButton = new JButton("=");
-		JButton divideButton = new JButton("/");
-		buttonPanel.add(oneButton);
-		buttonPanel.add(twoButton);
-		buttonPanel.add(threeButton);
-		buttonPanel.add(addButton);
-		buttonPanel.add(fourButton);
-		buttonPanel.add(fiveButton);
-		buttonPanel.add(sixButton);
-		buttonPanel.add(subtractButton);
-		buttonPanel.add(sevenButton);
-		buttonPanel.add(eightButton);
-		buttonPanel.add(nineButton);
-		buttonPanel.add(multiplyButton);
-		buttonPanel.add(resetButton);
-		buttonPanel.add(zeroButton);
-		buttonPanel.add(calculateButton);
-		buttonPanel.add(divideButton);
+
+		int length = buttonLabelsInOrder.length();
+		for (int i = 0; i < length; i++) {
+			JButton newButton = new JButton(String.valueOf(buttonLabelsInOrder.charAt(i)));
+			newButton.addMouseListener(this);
+			buttons.add(newButton);
+			buttonPanel.add(newButton);
+		}
 		frame.add(buttonPanel);
-		
-		oneButton.addMouseListener(this);
-		twoButton.addMouseListener(this);
-		threeButton.addMouseListener(this);
-		addButton.addMouseListener(this);
-		fourButton.addMouseListener(this);
-		fiveButton.addMouseListener(this);
-		sixButton.addMouseListener(this);
-		subtractButton.addMouseListener(this);
-		sevenButton.addMouseListener(this);
-		eightButton.addMouseListener(this);
-		nineButton.addMouseListener(this);
-		multiplyButton.addMouseListener(this);
-		resetButton.addMouseListener(this);
-		zeroButton.addMouseListener(this);
-		calculateButton.addMouseListener(this);
-		divideButton.addMouseListener(this);
 	}
 
 	private void createOutputField(JPanel frame) {
@@ -103,13 +66,10 @@ public class CalculatorGui implements MouseListener{
 	}
 
 	public void mouseClicked(MouseEvent e) {}
-
 	public void mouseEntered(MouseEvent e) {}
-
 	public void mouseExited(MouseEvent e) {}
-
 	public void mousePressed(MouseEvent e) {}
-
+	
 	public void mouseReleased(MouseEvent e) {
 		if(e.getSource() instanceof JButton){
 			char digit = ((JButton) e.getSource()).getText().charAt(0);
